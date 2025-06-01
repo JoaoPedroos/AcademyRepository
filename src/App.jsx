471ea4659c4e19eb702d48d01b4a25f5ac7491a2
João Pedro <107579744+JoaoPedroos@users.noreply.github.com>
2025-06-01 00:56:58 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import LandingPage from "./components/LandingPage";
+import { useEffect } from "react";
 
-export default function App() {
-  return <LandingPage />;
+function App() {
+  fetch('https://backend-repo-grk7.onrender.com/registrar-visita', { method: 'POST' });
+  fetch('https://backend-repo-grk7.onrender.com/registrar-interesse', { method: 'POST' });
+  return (
+    <div>
+      <LandingPage />;
+    </div>
+  );
 }
+
+export default App;
